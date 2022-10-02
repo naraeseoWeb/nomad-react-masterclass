7c11b64e6973dfda8e20b9a131630931f21527e3
naraeseo <naraeseoez@gmail.com>
2022-10-03 08:47:21 +0900

    #2.2: styled components 응용1 (props를 사용해서 컴포넌트에서 속성 결정하기, 상속받기)

--- src/App.js
+++ src/App.js
@@ -4,28 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 200px;
+  height: 200px;
 `;
 
-const Text = styled.span`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 100px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor='teal' />
+      <Circle bgColor='tomato' />
     </Father>
   );
 }

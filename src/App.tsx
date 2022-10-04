beb3496eeccf83350732edfd5569f02b3cf54bb3
naraeseo <naraeseoez@gmail.com>
2022-10-04 13:22:38 +0900

    #3.7 TypeScript : Recap

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -8,11 +8,25 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text}</H1>;
+}
+
 function App() {
+  // 같은 버튼 온클릭 이벤트라고 하더라도 form 내부에 있는지 여부에 따라 MouseEvent/FormEvent 차이가 남
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
+
   return (
     <div>
       <Container>
-        <H1>Protected</H1>
+        <Dummy active text='hello' />
+        {/* <Dummy active={true} text='hello' /> */}
+        <button onClick={onClick}>Click me</button>
       </Container>
     </div>
   );

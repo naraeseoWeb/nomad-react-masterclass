38287b3df50d429a0621a9912caec05c4a2189af
naraeseo <naraeseoez@gmail.com>
2022-10-04 12:49:21 +0900

    #3.6 TypeScript : Theme 만들기(/styled component)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { darkTheme, lightTheme } from './theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);

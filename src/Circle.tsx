ed1f0fec5f7640b44ed0ce4addfabf3f85952b6c
naraeseo <naraeseoez@gmail.com>
2022-10-04 12:01:02 +0900

    #3.3 TypeScript : optinal props사용, 디폴트 값 넣기

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 interface ContainerProps {
   bgColor: string;
+  borderColor?: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,28 +10,25 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-const Circle = ({ bgColor }: CircleProps) => {
+const Circle = ({
+  bgColor,
+  borderColor,
+  text = 'default text',
+}: CircleProps) => {
   return (
-    <>
-      <Container bgColor={bgColor} />
-    </>
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
   );
 };
 
 export default Circle;
-
-interface PlayerShape {
-  name: string;
-  age: number;
-}
-
-const sayHello = (playerObj: PlayerShape) =>
-  `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
-
-console.log(sayHello({ name: 'Mike', age: 2 }));

cbaadf4a7e3d4e080499acb9d80b0bb1737267ad
naraeseo <naraeseoez@gmail.com>
2022-10-06 18:59:49 +0900

    #4.13 : ApexCharts를 이용해 차트 만들기 1

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,16 +1,70 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchCoinHistory } from '../api';
+import ApexChart from 'react-apexcharts';
 
 interface ChartProps {
   coinId: string;
 }
 
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 const Chart = ({ coinId }: ChartProps) => {
-  const { isLoading, data } = useQuery(['ohlcv', coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(['ohlcv', coinId], () =>
     fetchCoinHistory(coinId)
   );
 
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        'Loading...'
+      ) : (
+        <ApexChart
+          type='line'
+          series={[
+            {
+              name: 'Price',
+              data: data?.map((price) => Number(price.close)) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: 'dark',
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: 'transparent',
+            },
+            grid: { show: false },
+            stroke: {
+              curve: 'smooth',
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisTicks: { show: false },
+              axisBorder: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Chart;

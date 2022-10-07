b128f4d924d95596b9e93728a36951c2ddc6ad18
naraeseo <naraeseoez@gmail.com>
2022-10-07 17:43:53 +0900

    #4+@ : 링크 minor 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,7 +7,7 @@ const Router = () => {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/:coinId/*' element={<Coin />}></Route>
-        <Route path={`/`} element={<Coins />}></Route>
+        <Route path={'/'} element={<Coins />}></Route>
       </Routes>
     </BrowserRouter>
   );

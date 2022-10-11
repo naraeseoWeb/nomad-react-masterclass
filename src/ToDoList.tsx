0ecbf4e2b752914780eca9b59721f01dcf82bb43
naraeseo <naraeseoez@gmail.com>
2022-10-11 09:21:11 +0900

    #5.6 : Form 관리(react hook form 없는 버전)

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,23 +1,29 @@
 import React, { useState } from 'react';
 
 const ToDoList = () => {
-  const [todo, setTodo] = useState('');
+  const [toDo, setTodo] = useState('');
+  const [toDoError, setToDoError] = useState('');
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
+    setToDoError('');
     setTodo(value);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(todo, 'todo');
+    if (toDo.length < 10) {
+      return setToDoError('To do should be longer');
+    }
+    console.log('submit');
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input value={todo} onChange={onChange} placeholder='Write a to do' />
+        <input value={toDo} onChange={onChange} placeholder='Write a to do' />
         <button>Add</button>
+        {toDoError !== '' ? toDoError : null}
       </form>
     </div>
   );

25240d15bc96c92ed71bd59f6bb998ac202b1691
naraeseo <naraeseoez@gmail.com>
2022-10-11 09:46:21 +0900

    #5.6 : Form 관리(react hook form 로그인 form 예시)

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+// import React, { useState  } from 'react';
 
-const ToDoList = () => {
+import { useForm } from 'react-hook-form';
+
+/* const ToDoList = () => {
   const [toDo, setTodo] = useState('');
   const [toDoError, setToDoError] = useState('');
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
@@ -24,6 +26,24 @@ const ToDoList = () => {
         <input value={toDo} onChange={onChange} placeholder='Write a to do' />
         <button>Add</button>
         {toDoError !== '' ? toDoError : null}
+      </form>
+    </div>
+  );
+}; */
+
+const ToDoList = () => {
+  const { register, watch } = useForm();
+  console.log(watch());
+  return (
+    <div>
+      <form>
+        <input {...register('email')} placeholder='Email' />
+        <input {...register('firstName')} placeholder='First Name' />
+        <input {...register('lastName')} placeholder='Last Name' />
+        <input {...register('username')} placeholder='User Name' />
+        <input {...register('password')} placeholder='Password' />
+        <input {...register('password1')} placeholder='Password 1' />
+        <button>Add</button>
       </form>
     </div>
   );

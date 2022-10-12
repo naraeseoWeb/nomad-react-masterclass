946a938844539e58ba64ed272c0b3ff823c37541
naraeseo <naraeseoez@gmail.com>
2022-10-12 16:14:30 +0900

    #5.15 To Do List: onClick 시 배열 새로 구성하기

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,19 +1,40 @@
-import { IToDo } from './atoms';
+import { useSetRecoilState } from 'recoil';
+import { IToDo, toDoState } from './atoms';
 
-const ToDo = ({ text, category }: IToDo) => {
-  const onClick = (newCategory: IToDo['category']) => {};
+const ToDo = ({ text, category, id }: IToDo) => {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
 
   return (
     <li>
       <span>{text}</span>
       {category !== 'TO_DO' && (
-        <button onClick={() => onClick('TO_DO')}>To Do</button>
+        <button name='TO_DO' onClick={onClick}>
+          To Do
+        </button>
       )}
       {category !== 'DOING' && (
-        <button onClick={() => onClick('DOING')}>Doing</button>
+        <button name='DOING' onClick={onClick}>
+          Doing
+        </button>
       )}
       {category !== 'DONE' && (
-        <button onClick={() => onClick('DONE')}>Done</button>
+        <button name='DONE' onClick={onClick}>
+          Done
+        </button>
       )}
     </li>
   );

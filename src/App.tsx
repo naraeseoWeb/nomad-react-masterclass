920c36893f8ea11f890da90a712cdf1300cc49e7
naraeseo <naraeseoez@gmail.com>
2022-10-14 14:12:09 +0900

    #6.2 [칸반보드] : reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
 import React from 'react';
-import { useRecoilState } from 'recoil';
-import { hourSelector, minuteState } from './atoms';
+import { DragDropContext } from '/react-beautiful-dnd';
 
 const App = () => {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-  };
-  const onHourChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
-
-  return (
-    <div>
-      <input
-        value={minutes}
-        onChange={onMinuteChange}
-        type='number'
-        placeholder='Minutes'
-      />
-      <input
-        value={hours}
-        onChange={onHourChange}
-        type='number'
-        placeholder='Hours'
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
